731fefaa1478f650e119363e39827070b424941b
dai4 <dragosionescuu@gmail.com>
2013-11-08 15:00:36 +0000

    Made the modal menu responsive
    - Modal Menu is now responsive up to 780px
    - Fixed some weird padding with the other divs
    - Reworked the way the modal menu works
    - Made the Grey Bar responsive as well

--- js/kentUnionJs.js
+++ js/kentUnionJs.js
@@ -1,8 +1,8 @@
-var modal_campuses = '<div id="panelcontent" class="col-centered panelcontent" style=""> <div class="panelheight"> <div class="grid_left_column"> <div class="intropanel"> <h2 class = "museoTitle">Other Campuses</h2> <p class="museoSmallTitle">Hey, we\'re not just in Canterbury! </p> <p><strong>We have campuses all over the UK and Europe too! Pick the campus you are interested in on the right.</strong></p> </div> </div> <div class="grid_right_column"><a class="close-reveal-modal">×</a><div class="bottom"><ul id="campuses"><li class="clearfix"><a href="http://www.kent.ac.uk/locations/"><img alt="Brussels Campus" src="http://kentunion.co.uk/stylesheet/kentunionresponsivedesign/brussels.png"><p>Satellite sites</p></a></li><li class="clearfix"><a href="http://www.umsa.org.uk/"><img alt="Medway Campus" src="http://kentunion.co.uk/stylesheet/kentunionresponsivedesign/medway.png"><p>Medway</p></a></li><li class="clearfix"><a href="http://www.kentunion.co.uk/"><img alt="Canterbury Campus" src="http://kentunion.co.uk/stylesheet/kentunionresponsivedesign/canterbury.png"><p>Canterbury</p></a></li></ul> </div> </div> </div>  </div>';
+var modal_campuses = '<div id="panelcontent" class="col-centered panelcontent" style=""> <div class="panelheight"> <div class="grid_left_column" id="campusL"> <div class="intropanel"> <h2 class = "museoTitle">Other Campuses</h2> <p class="museoSmallTitle">Hey, we\'re not just in Canterbury! </p> <p><strong>We have campuses all over the UK and Europe too! Pick the campus you are interested in on the right.</strong></p> </div> </div> <div class="grid_right_column" id="campusR"><div class="bottom" id="campusB"><ul id="campuses"><li class="clearfix"><a href="http://www.kent.ac.uk/locations/"><img alt="Brussels Campus" src="http://kentunion.co.uk/stylesheet/kentunionresponsivedesign/brussels.png"><p>Satellite sites</p></a></li><li class="clearfix"><a href="http://www.umsa.org.uk/"><img alt="Medway Campus" src="http://kentunion.co.uk/stylesheet/kentunionresponsivedesign/medway.png"><p>Medway</p></a></li><li class="clearfix"><a href="http://www.kentunion.co.uk/"><img alt="Canterbury Campus" src="http://kentunion.co.uk/stylesheet/kentunionresponsivedesign/canterbury.png"><p>Canterbury</p></a></li></ul> </div> </div> </div>  </div>';
 
-var modal_services = '<div id="panelcontent" class="col-centered panelcontent" style=""> <div class="panelheight"> <div class="grid_left_column"> <div class="intropanel"> <h2 class = "museoTitle">Services</h2> <p class="museoSmallTitle">We provide loads of services, not just The Venue &amp; Essentials! </p>  <p class="bold">All of our services are aimed at making your time at Uni better, from the nursery to the advice centre.</p> <p><strong>Whatever you need, we\'ve got your back!</strong></p> </div> </div>  <div class="grid_right_column"><a class="close-reveal-modal">×</a> <div class="bottom"><div class="grid_content_colum"> <ul class="services_links"> <li><a href="http://www.kentunion.co.uk/advice">Advice Centre</a></li> <li><a href="http://www.kentunion.co.uk/services/essentials">Essentials</a></li> <li><a href="http://www.kentunion.co.uk/jobs">Jobshop</a></li> <li><a href="http://www.kentunion.co.uk/services/librarycafe">Library Café</a></li> <li><a href="http://www.oaksdaynursery.co.uk/">Oaks Day Nursery</a></li> <li><a href="http://www.kentunion.co.uk/services/pwessentials">Park Wood Essentials</a></li> </ul></div><div class="grid_content_colum pad"> <ul class="services_links"> <li><a href="http://www.kentunion.co.uk/services/passportphotoservice/">Passport Photo Service</a></li> <li><a href="http://www.kentunion.co.uk/services/post">Postal Service</a></li><li><a href="http://www.kentunion.co.uk/services/printshop">Print Shop</a></li><li><a href="http://www.kentunion.co.uk/societies">Societies</a></li> <li><a href="http://www.kentunion.co.uk/sports">Sports</a></li><li><a href="http://www.kentunion.co.uk/studentmedia">Student Media</a></li> </ul></div><div class="grid_content_colum pad"> <ul class="services_links"> <li><a href="http://www.kentunion.co.uk/thevenue">The Venue</a></li><li><a href="http://www.kentunion.co.uk/unique">Unique</a></li><li><a href="http://www.kentunion.co.uk/volunteering">Volunteering</a></li> <li><a href="http://www.kentunion.co.uk/woodys">Woodys</a></li></ul></div></div></div></div></div>';
+var modal_services = '<div id="panelcontent" class="col-centered panelcontent" style=""> <div class="panelheight"> <div class="grid_left_column" id="servicesL"> <div class="intropanel"> <h2 class = "museoTitle">Services</h2> <p class="museoSmallTitle">We provide loads of services, not just The Venue &amp; Essentials! </p>  <p class="bold">All of our services are aimed at making your time at Uni better, from the nursery to the advice centre.</p> <p><strong>Whatever you need, we\'ve got your back!</strong></p> </div> </div>  <div class="grid_right_column" id="servicesBR"> <div class="bottom clearfix" id="servicesR"><div class="grid_content_colum"> <ul class="services_links"> <li><a href="http://www.kentunion.co.uk/advice">Advice Centre</a></li> <li><a href="http://www.kentunion.co.uk/services/essentials">Essentials</a></li> <li><a href="http://www.kentunion.co.uk/jobs">Jobshop</a></li> <li><a href="http://www.kentunion.co.uk/services/librarycafe">Library Café</a></li> <li><a href="http://www.oaksdaynursery.co.uk/">Oaks Day Nursery</a></li> <li><a href="http://www.kentunion.co.uk/services/pwessentials">Park Wood Essentials</a></li> </ul></div><div class="grid_content_colum pad"> <ul class="services_links"> <li><a href="http://www.kentunion.co.uk/services/passportphotoservice/">Passport Photo Service</a></li> <li><a href="http://www.kentunion.co.uk/services/post">Postal Service</a></li><li><a href="http://www.kentunion.co.uk/services/printshop">Print Shop</a></li><li><a href="http://www.kentunion.co.uk/societies">Societies</a></li> <li><a href="http://www.kentunion.co.uk/sports">Sports</a></li><li><a href="http://www.kentunion.co.uk/studentmedia">Student Media</a></li> </ul></div><div class="grid_content_colum pad"> <ul class="services_links"> <li><a href="http://www.kentunion.co.uk/thevenue">The Venue</a></li><li><a href="http://www.kentunion.co.uk/unique">Unique</a></li><li><a href="http://www.kentunion.co.uk/volunteering">Volunteering</a></li> <li><a href="http://www.kentunion.co.uk/woodys">Woodys</a></li></ul></div></div></div></div></div>';
 
-var modal_cookies = '<div id="panelcontent" class="col-centered panelcontent" style=""> <div class="panelheight"> <div class="grid_left_column"> <div class="intropanel"> <h2 class = "museoTitle">Cookies</h2> <p class="museoSmallTitle">OMNOMNOMNOM Cookies!</p>  <span id="cookies-picture"></span> </div> </div>  <div class="grid_right_column"><a class="close-reveal-modal">×</a><div class="bottom"><p class="full-notice"><strong>We try our hardest not to track anything you do on our site</strong>. Unfortunately, we kind of have to. </p><p>When you log in and click \'remember me\', that\'s a cookie. When you visit a page on the site we use something called Google Analytics (it tells us what pages you visit the most and information like which web browser you are using). That\'s also a cookie.</p><p> Those are pretty much the main cookies on our site. There are a couple more that pop up when we have facebook intergration or other cool things to make the website better. <strong>We do all of this with the best of intentions but wanted to let you know</strong>.</p><p>For more information about all our privacy and cookies policies <a href="http://kentunion.co.uk/privacy/">click here</a>.</p></div> </div> </div>	 </div>'
+var modal_cookies = '<div id="panelcontent" class="col-centered panelcontent" style=""> <div class="panelheight"> <div class="grid_left_column"> <div class="intropanel"> <h2 class = "museoTitle">Cookies</h2> <p class="museoSmallTitle">OMNOMNOMNOM Cookies!</p>  <span id="cookies-picture"></span> </div> </div>  <div class="grid_right_column"><div class="bottom"><p class="full-notice"><strong>We try our hardest not to track anything you do on our site</strong>. Unfortunately, we kind of have to. </p><p>When you log in and click \'remember me\', that\'s a cookie. When you visit a page on the site we use something called Google Analytics (it tells us what pages you visit the most and information like which web browser you are using). That\'s also a cookie.</p><p> Those are pretty much the main cookies on our site. There are a couple more that pop up when we have facebook intergration or other cool things to make the website better. <strong>We do all of this with the best of intentions but wanted to let you know</strong>.</p><p>For more information about all our privacy and cookies policies <a href="http://kentunion.co.uk/privacy/">click here</a>.</p></div> </div> </div>	 </div>'
 
 
 $(document).ready(function() {
@@ -10,6 +10,7 @@ $(document).ready(function() {
    // Initialize banner carousel
     $("#homepage-carousel").owlCarousel({      
         autoPlay: 5000,
+        autoHeight : true,
         pagination: false,
         slideSpeed : 300,
         paginationSpeed : 400,
@@ -61,11 +62,37 @@ $(document).ready(function() {
         });
     });
     
-    $('#modal_OuterContainer').on('click','.close-reveal-modal', function() {
+    $('.close-reveal-modal').click(function() {
         $('.modal').slideUp('fast', function() {
             $('#overlay').fadeOut('fast');
             $('#greyBar li a').removeClass('greyBar_active');
             $('#modal_OuterContainer').html('');
         });
+    });
+    
+    $('#main-homepage-social').css('height', $('#main-homepage-images').outerHeight());
+    var windowWidth_start = $(window).outerWidth();
+    if(windowWidth_start > 1140) {
+            $('#greyBar div').removeClass('col-md-offset-3');
+            $('#greyBar div').addClass('col-md-offset-5');
+        }
+    else if(windowWidth_start < 1140) {
+            $('#greyBar div').removeClass('col-md-offset-5');
+            $('#greyBar div').addClass('col-md-offset-3');
+        }
+
+    
+    $(window).resize(function() {
+        var windowWidth = $(window).outerWidth();
+        var imageHeight = $('#main-homepage-images').outerHeight();
+        $('#main-homepage-social').css('height', imageHeight);
+        if(windowWidth > 1140) {
+            $('#greyBar div').removeClass('col-md-offset-3');
+            $('#greyBar div').addClass('col-md-offset-5');
+        }
+        else if(windowWidth < 1140) {
+            $('#greyBar div').removeClass('col-md-offset-5');
+            $('#greyBar div').addClass('col-md-offset-3');
+        }
     });
 });
